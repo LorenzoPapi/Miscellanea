b622d0b6f294e26903cc6d81087a9b7d0d94100b
LorenzoPapi <topolino123654@gmail.com>
2023-10-29 12:35:25 +0100

    Karnaugh map

---
+++ Esoterici/Beatnik/calc.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Calcolatrice in Beatnik!" << endl;
+    cout << "Inserire primo valore" << endl;
+    double a = 0;
+    cin >> a;
+    
+    return 0;
+}
--- Esoterici/Beatnik/interpreter.cpp
+++ Esoterici/Beatnik/interpreter.cpp
@@ -4,10 +4,11 @@
 #include <stack>
 #include <vector>
 using namespace std;
+typedef long long ll;
 
-stack<int> S;
-vector<int> instructions;
-long parse_index = 0;
+stack<ll> S;
+vector<ll> instructions;
+ll parse_index = 0;
 
 void interpret();
 
@@ -19,7 +20,7 @@ int main() {
 
     // PARSING
     char ch;
-    int score = 0;
+    ll score = 0;
     int scrabble[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
     if (text.is_open()) {
         while (text >> noskipws >> ch) {
@@ -33,8 +34,6 @@ int main() {
     }
     text.close();
     
-    for (char c : "Calcolatrice in Beatnik") cout << int(c) << "; ";
-    cout << endl;
     //INTERPRETING
     S.push(0);
     while (parse_index < instructions.size()) {
@@ -45,93 +44,86 @@ int main() {
     return 0;
 }
 
-int get_and_pop() {
+ll get_and_pop() {
     if (S.size() == 0) {
-        cout << "ERROR: You are trying to pop an empty stack at " << parse_index << ". Check your poem." << endl;
+        cout << endl << "ERROR: POP with an empty stack at " << parse_index << ". Check your poem." << endl;
         exit(-1);
     } else {
-        if (S.size() == 1) cout << "WARNING: You are completely emptying the stack with a POP operation at " << parse_index << ". While this may be intentional, it usually means that your code is broken. " << endl;
-        int p = S.top(); S.pop();
+        if (S.size() == 1) cout << endl << "WARNING: You are completely emptying the stack with a POP operation at " << parse_index << ". While this may be intentional, it usually means that your code is broken. " << endl;
+        ll p = S.top(); S.pop();
         return p;
     }
 }
 
+void arg_command(int code) {
+    //map code->name
+    ll old_index = parse_index++;
+    if (parse_index == instructions.size() - 1) cout << endl << "WARNING: " << code << "instruction at the end of the file; will be ignored." << endl;
+    else {
+        switch (code) {
+            case 5:
+                S.push(instructions[parse_index]);
+                break;
+            case 13:
+            //CONTINUA DA QUI SCEM
+                if (get_and_pop() == 0) parse_index = old_index + instructions[parse_index] - 1;
+                if (parse_index >= instructions.size()) cout << endl << "WARNING: N_FORWARD_IF_ZERO went beyond the instruction limit by " << (parse_index - instructions.size() - 1) << " at index " << old_index <<". While this may be intentional, it could create bugs." << endl;
+                break;
+            case 14:
+                if (get_and_pop() != 0) parse_index = old_index + instructions[parse_index] - 1;
+                if (parse_index >= instructions.size()) cout << endl << "WARNING: N_FORWARD_IFN_ZERO went beyond the instruction limit by " << (parse_index - instructions.size() - 1) << " at index " << old_index <<". While this may be intentional, it could create bugs." << endl;
+                break;
+            case 15:
+            case 16:
+                if ((get_and_pop() == 0 && code == 15) || (get_and_pop() != 0 && code == 16)) parse_index = old_index - instructions[parse_index] - 1;
+                if (parse_index < -1) cout << endl << "WARNING: " << code << " went less than zero by"  << (-parse_index + 1) << " at index " << old_index <<". While this may be intentional, it could create bugs." << endl;
+                break;
+            default:
+                break;
+        }
+    }
+}
+
 void interpret() {
-    switch (instructions[parse_index])
-    {
-    case 5: {
-        if (parse_index == instructions.size() - 1) cout << "WARNING: PUSH instruction at the end of the file; will be ignored." << endl;
-        else S.push(instructions[++parse_index]);
-        break;
-    }
-    case 6: {
-        get_and_pop();
-        break;
-    }
-    case 7: {
-        int a = get_and_pop();
-        int b = get_and_pop();
-        S.push(a + b);
-        break;
-    }
-    case 8: {
-        int input;
-        cin >> input;
-        S.push(input);
-        break;
-    }
-    case 9: { 
-        int c = get_and_pop();
-        cout << char(c);
-        break;
-    }
-    case 10: {
-        int a = get_and_pop();
-        int b = get_and_pop();
-        S.push(a - b);
-        break;
-    }
-    case 11: {
-        int a = get_and_pop();
-        int b = get_and_pop();
-        S.push(b); S.push(a);
-        break;
-    }
-    case 12: {
-        //check if it works
-        S.push(S.top());
-        break;
-    }
-    case 13: {
-        int old_ind = parse_index;
-        if (get_and_pop() == 0) parse_index += instructions[++parse_index];
-        if (parse_index >= instructions.size()) cout << "WARNING: N_FORWARD_IF_ZERO went beyond the instruction limit by " << (parse_index - instructions.size() - 1) << " at index " << old_ind <<". While this may be intentional, it could create bugs." << endl;
-        break;
-    }
-    case 14: {
-        int old_ind = parse_index;
-        if (get_and_pop() != 0) parse_index += instructions[++parse_index];
-        if (parse_index >= instructions.size()) cout << "WARNING: N_FORWARD_IFN_ZERO went beyond the instruction limit by " << (parse_index - instructions.size() - 1) << " at index " << old_ind <<". While this may be intentional, it could create bugs." << endl;
-        break;
-    }
-    case 15: {
-        int old_ind = parse_index;
-        if (get_and_pop() == 0) parse_index -= instructions[++parse_index];
-        if (parse_index < 0) cout << "WARNING: N_BACK_IF_ZERO went less than zero by " << (-parse_index) << " at index " << old_ind <<". While this may be intentional, it could create bugs." << endl;
-        break;
-    }
-    case 16: {
-        int old_ind = parse_index;
-        if (get_and_pop() != 0) parse_index -= instructions[++parse_index];
-        if (parse_index < 0) cout << "WARNING: N_BACK_IFN_ZERO went less than zero by " << (-parse_index) << " at index " << old_ind <<". While this may be intentional, it could create bugs." << endl;
-        break;
-    }
-    case 17: {
-        exit(0);
-        break;
-    }
-    default:
-        break; //NOOP
+    switch (instructions[parse_index]) {
+        case 5:
+        case 13:
+        case 14:
+        case 15:
+        case 16:
+            arg_command(instructions[parse_index]);
+            break;
+        case 6:
+            get_and_pop();
+            break;
+        case 7:
+            S.push(get_and_pop() + get_and_pop());
+            break;
+        case 8:
+            char input;
+            cin >> input;
+            S.push(input);
+            break;
+        case 9: 
+            cout << char(get_and_pop());
+            break;
+        case 10:
+            S.push(get_and_pop() - get_and_pop());
+            break;
+        case 11: {
+            ll a = get_and_pop();
+            ll b = get_and_pop();
+            S.push(b); S.push(a);
+            break;
+        }
+        case 12:
+            S.push(S.top());
+            break;
+        case 17:
+            exit(0);
+            break;
+        default:
+            break; //NOOP
     }
 }
 
